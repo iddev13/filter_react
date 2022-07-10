d1ec5a17af2c90f4de7b02ff6001ba4201be2723
Igor <80629173+iddev13@users.noreply.github.com>
2022-07-10 13:55:17 +0300

    1.0.1

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,32 +1,52 @@
 import './Filter.css';
+import { motion, AnimatePresence } from "framer-motion/dist/framer-motion"
 
 const Filter = ({ filterFunc, menuItem, buttons }) => {
+
+
 	return (
 		<div className="filter">
 			<div className="filter__buttons">
 				{
 					buttons.map((elem, index) => {
-						return <button type="button" onClick={() => filterFunc(elem)} className="filter__btn" key={index.toString()}>{elem}</button>
+						return <button
+							type="button"
+							onClick={() => filterFunc(elem)}
+							className="filter__btn" key={index.toString()}
+						>{elem}</button>
 					})
 				}
 			</div>
 
-			<div className="filter__list">
-				{
-					menuItem.map((item) => {
-						return <div className="filter__item" key={item.id}>
-							<div className="filter__item-body">
-								<div className="filter__item-image-btn" >
-									<a href="/"></a>
-									<img src={item.image} alt="" />
+			<motion.div
+				layout
+				className="filter__list">
+				<AnimatePresence>
+
+					{
+						menuItem.map((item) => {
+							return <motion.div
+								// animate={{ y: 100 }}
+								animate={{ opacity: 1 }}
+								initial={{ opacity: 0 }}
+								exit={{ opacity: 0 }}
+								layout
+								className="filter__item"
+								key={item.id}>
+								<div className="filter__item-body">
+									<div className="filter__item-image-btn" >
+										<a href="#">
+											<img src={item.image} alt="Alt" />
+										</a>
+									</div>
+									<h2>{item.title}</h2>
+									<p>{item.description}</p>
 								</div>
-								<h2>{item.title}</h2>
-								<p>{item.description}</p>
-							</div>
-						</div>
-					})
-				}
-			</div>
+							</motion.div>
+						})
+					}
+				</AnimatePresence>
+			</motion.div>
 		</div>
 	)
 }
